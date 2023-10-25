7463e067e6f3011af86e6b808a84109aeecebfb0
rafaelst2000 <rafaelst2000@gmail.com>
2023-10-25 14:24:45 -0300

    style: run lint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="Plataforma online para compartilhamento de roteiros entre mototuristas" />
+        <meta
+          name="description"
+          content="Plataforma online para compartilhamento de roteiros entre mototuristas"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <style
